00ae1f2c60596fec04549d9a5c8bb537c6c9ca44
Marlon Carrion <marlon.carrion.est@tecazuay.edu.ec>
2020-09-15 00:10:47 -0500

    Creación de page bienvenida

--- gestoragua/src/app/app-routing.module.ts
+++ gestoragua/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./bienvenida/bienvenida.module').then( m => m.BienvenidaPageModule)
+  },
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
